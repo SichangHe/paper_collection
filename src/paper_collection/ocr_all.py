9bb08c6b857def05203357106dc0c97fa93ad164
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-09-04 17:53:03 -0700

    initial attempt to OCR all papers

---
+++ src/paper_collection/ocr_all.py
@@ -0,0 +1,54 @@
+"""Find all PDFs that do not have a corresponding text file and run OCR on
+them.
+"""
+
+import logging
+import os
+import subprocess
+from logging import getLogger
+from pathlib import Path
+
+logging.basicConfig(level="INFO")
+logger = getLogger(__name__)
+
+
+def ocr_pdf(pdf: Path):
+    # Run `nougat {pdf} -m 0.1.0-base --full-precision --no-skipping`
+    output = subprocess.run(
+        ["nougat", str(pdf), "-m", "0.1.0-base", "--full-precision", "--no-skipping"],
+        capture_output=True,
+        text=True,
+    )
+    if output.returncode != 0:
+        logger.error("Failed to process `%s`: %s", pdf, output.stderr)
+        return None
+    return output.stdout
+
+
+def process_pdf(pdf_path: Path):
+    md_path = pdf_path.with_suffix(".md")
+    if md_path.exists():
+        logger.info("Skipping `%s` → `%s`", pdf_path, md_path)
+        return 0
+    else:
+        logger.warning("Starting OCR `%s` → `%s`", pdf_path, md_path)
+        markdown = ocr_pdf(pdf_path)
+        if markdown is None:
+            logger.error("Did not write to `%s`", md_path)
+            return 1
+        else:
+            md_path.write_text(markdown)
+            logger.info("Wrote output to `%s`", md_path)
+            return 0
+
+
+def main():
+    failures = 0
+    for pdf_name in os.listdir():
+        pdf_path = Path(pdf_name)
+        if pdf_path.suffix == ".pdf":
+            failures += process_pdf(pdf_path)
+    return failures
+
+
+exit(main()) if __name__ == "__main__" else None
